95f29dbb9f1b2861a36d0d075026481c3b6f67fd
Jakub <jakub.kicmal@gmail.com>
2023-02-18 15:40:26 +0100

    react-query-02

--- react-query-02/tutorial/README.md
+++ react-query-02/tutorial/README.md
@@ -2,4 +2,7 @@
 
 ```
 git add . && git commit -m "react-material-ui-01" && git push
+
+yarn add @tanstack/react-query
+yarn add -D @tanstack/react-query-devtools
 ```
--- react-query-02/tutorial/src/App.tsx
+++ react-query-02/tutorial/src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+// /posts -> ["posts"]
+// /posts/1 -> ["posts", post.id]
+// /posts?authorId=1 -> ["posts", { authorId: 1}]
+// /posts/2/comments -> ["posts", post.id, "comments"]
+
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 2" },
+] as { id: number | string; title: string }[];
 
 function App() {
+  console.log(POSTS);
+
+  const queryClient = useQueryClient();
+
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: ({ queryKey }) => {
+      console.log({ queryKey });
+
+      return wait(1000).then(() => [...POSTS]);
+    },
+    // queryFn: () => Promise.reject("Error Message"),
+  });
+
+  console.log(postsQuery.isFetching);
+
+  const newPostMutation = useMutation({
+    mutationFn: (title: string) =>
+      wait(1000).then(() =>
+        POSTS.push({
+          id: crypto.randomUUID(),
+          title,
+        })
+      ),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["posts"]);
+    },
+  });
+
+  if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  if (postsQuery.error) return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {postsQuery.data?.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
+      <button
+        disabled={newPostMutation.isLoading}
+        onClick={() => newPostMutation.mutate("New Post")}
+      >
+        Add New
+      </button>
     </div>
   );
+}
+
+function wait(duration: number) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
 }
 
 export default App;
